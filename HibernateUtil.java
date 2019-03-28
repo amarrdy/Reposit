4e8c3f0d7d5c6a09138af0767ec25c5491336260
Amar <amarrdy93@gmail.com>
2019-03-28 17:14:33 +0530

    added util file

---
+++ HibernateUtil.java
@@ -0,0 +1,19 @@
+package com.jpspiders.hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private final static SessionFactory factory;
+	
+	static {
+		Configuration cfg = new Configuration();
+		cfg.configure();
+		factory = cfg.buildSessionFactory();
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return factory;
+	}
+}
