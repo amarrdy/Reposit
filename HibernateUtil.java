f25ea688fb1df53dbb1f66d0ef2941b1827fcc75
Amar <amarrdy93@gmail.com>
2019-03-28 17:16:22 +0530

    modified file added

--- HibernateUtil.java
+++ HibernateUtil.java
@@ -3,6 +3,8 @@ package com.jpspiders.hibernate.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+// hibernate files are uploaded
+
 public class HibernateUtil {
 
 	private final static SessionFactory factory;
